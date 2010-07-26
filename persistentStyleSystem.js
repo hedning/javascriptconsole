b12865eb40384fedaeb031b155d2ae94adf079c3
Tor Hedin Brønner <torhedinbronner@gmail.com>
2010-07-27 01:39:22 +0200

    added system for storing and applying styles
    uses localStorage to store styles for specific elements

---
+++ persistentStyleSystem.js
@@ -0,0 +1,67 @@
+
+//function () {
+
+
+function getPersistentStyles() {
+
+	persistentStyles = new Array;
+	ruleCount = 0;
+	var i=0;
+	while ( localStorage["persistentStyleElement"+i] ) {
+
+		var element = dereferenceChildNodeTree(eval(localStorage["persistentStyleElement"+i]));
+		var style = localStorage["persistentStyle"+i];
+		var urlRule = null;
+
+		if ( localStorage["persistentUrlRule"+i] )
+			urlRule = eval(localStorage["persistentUrlRule"+i]);
+		if (element) {
+			persistentStyles.push( { element : element , urlRule : urlRule , style : style } );
+		}
+
+		i++;
+	}
+
+	ruleCount = persistentStyles.length;
+
+}
+
+function applyStyles() {
+
+	getPersistentStyles();
+
+	var element;
+	var style;
+	var urlRule;
+
+	for ( var i=0; i < persistentStyles.length; i++ ) {
+
+		opera.postError(persistentStyles[i].element+"\n"+ persistentStyles[i].element.constructor);
+		element = persistentStyles[i].element;
+		eval("style = " + persistentStyles[i].style);
+
+		for ( var k in style ) {
+			element.style[k] = style[k];
+		}
+	}
+}
+
+storeStyle = function (element, style) {
+
+	var treeId = "[" + buildChildNodeTree(element).toString() + "]";
+	var styleString = "{";
+
+	for ( var i in style ) {
+		styleString += i + ":'"+style[i]+"',";
+	}
+	styleString = styleString.replace(/,$/, "");
+	styleString += "}";
+
+	localStorage.setItem("persistentStyleElement"+ruleCount, treeId);
+	localStorage.setItem("persistentStyle"+ruleCount, styleString);
+	ruleCount++;
+}
+
+//document.addEventListener("load", applyStyles(), false);
+
+//} ()
