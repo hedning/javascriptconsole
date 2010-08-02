1ee3a95402db2f02d53eb5e0862b6bd8629ae187
Tor Hedin Brønner <torhedinbronner@gmail.com>
2010-08-03 01:24:42 +0200

    added keybindings.js

---
+++ keybindings.js
@@ -0,0 +1,9 @@
+defineBindings( 
+		{ bind: /h$/, action: function() {window.scrollBy(0,25)}, context: document.body },
+		{ bind: /t$/, action: function() {window.scrollBy(0,-25)}, context: document.body }
+//	  	{ bind: /<ctrl>u$/, action: scrollPageUp },
+//	  	{ bind: /<ctrl>a$/, action: moveToStartofLine, context: "textInput" },
+//	  	{ bind: /<esc>$/, action: actionSetMode("command") },
+//	 	{ bind: /i$/, action: actionSetMode("insert"), context: "textInput" },
+//		{ bind: /f(.?)$/, action: moveToChar, mode: "command", context: "textInput" } )
+)
