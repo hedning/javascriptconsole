31b23fa9a087b9cf775340dba663e340ec0eefa9
Tor Hedin Brønner <torhedinbronner@gmail.com>
2010-09-24 06:48:32 +0200

    Modules now have to be constructors (functions/classes)
    When importing a module you now get a copy for yourself.

--- modules.js
+++ modules.js
@@ -5,11 +5,14 @@ var modules = {};
 createModule = function (name, moduleObject) {
 	if ( modules[name] )
 		throw "Module already exist";
+	if ( typeof moduleObject !== "function" )
+		throw "Module '" +name+"' is not a constructor (function)";
+
 	return modules[name] = moduleObject;
 };
 
 importModule = function (name) {
-	return modules[name];
+	return new modules[name];
 };
 
 }());
