4b3d071d0e4ac5f1b0e5aad72b1711d2c6c2a6ca
Tor Hedin Brønner <torhedinbronner@gmail.com>
2010-10-07 23:41:24 +0200

    Removes the non css persistentStyles.
    I rarely used the stuff that didn't use css
    and this is a lot less code and cleaner.

--- persistentStyleSystem.js
+++ persistentStyleSystem.js
@@ -2,12 +2,9 @@
 
 var state = importModule("persistentState");
 var log = importModule("log").log;
-var persistentStyles = new Array;
 var persistentCssRules = state.getVariable("persistentCssRules") || [];
 var disableStyles = state.getVariable("disableStyles");
 disableStyles = disableStyles !== undefined ? disableStyles : false;
-var ruleCount = 0;
-
 
 function getLocalStylesheet() {
 	for ( var i=0; i < document.styleSheets.length; i++) {
@@ -54,70 +51,9 @@ function applyPersistentCss() {
 }
 applyPersistentCss();
 
-function getPersistentStyles() {
-
-	var i=0;
-	while ( localStorage["persistentStyleElement"+i] ) {
-
-		var element = dereferenceChildNodeTree(eval(localStorage["persistentStyleElement"+i]));
-		var style = localStorage["persistentStyle"+i];
-		var urlRule = null;
-
-		if ( localStorage["persistentUrlRule"+i] )
-			urlRule = eval(localStorage["persistentUrlRule"+i]);
-		if (element) {
-			persistentStyles.push( { element : element , urlRule : urlRule , style : style } );
-		}
-
-		i++;
-	}
-
-	ruleCount = persistentStyles.length;
-
-}
-
-var applyStyles = function () {
-
-	getPersistentStyles();
-
-	var element;
-	var style;
-	var urlRule;
-
-	for ( var i=0; i < persistentStyles.length; i++ ) {
-
-		log(persistentStyles[i].element+"\n"+ persistentStyles[i].element.constructor);
-		element = persistentStyles[i].element;
-		eval("style = " + persistentStyles[i].style);
-
-		for ( var k in style ) {
-			element.style[k] = style[k];
-		}
-	}
-//	applyPersistentCss();
-};
-
-var storeStyle = function (element, style) {
-
-	if ( type(element) == "String") {
-		persistentCssRules.push(element);
-		state.saveVariable("persistentCssRules", persistentCssRules);
-
-	} else {
-
-		var treeId = "[" + buildChildNodeTree(element).toString() + "]";
-		var styleString = "{";
-
-		for ( var i in style ) {
-			styleString += i + ":'"+style[i]+"',";
-		}
-		styleString = styleString.replace(/,$/, "");
-		styleString += "}";
-
-		localStorage.setItem("persistentStyleElement"+ruleCount, treeId);
-		localStorage.setItem("persistentStyle"+ruleCount, styleString);
-		ruleCount++;
-	}
+var storeStyle = function (rule) {
+	persistentCssRules.push(rule);
+	state.saveVariable("persistentCssRules", persistentCssRules);
 };
 
 var enableStyles = function (bool) {
@@ -130,10 +66,5 @@ createModule("userStyle", function(){
 		this.addUserCssRule = addUserCssRule;
 		this.enableStyles = enableStyles;
 });
-
-
-document.addEventListener("DOMContentLoaded", function() {
-	applyStyles();
-}, false)
 
 }());
